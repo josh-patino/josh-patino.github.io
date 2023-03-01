396f224cccf28b20af3f04e6050ee7659a8694e3
josh-patino <83191880+josh-patino@users.noreply.github.com>
2023-03-01 17:00:49 -0600

    resolved nav bar home issue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { ErrorComponent } from './error/error.component';
 
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent},
+  {path: '', component: HomeComponent},
   {path: 'projects', component: ProjectsComponent},
   {path:'hobbies', component: HobbiesComponent},
   {path: '**', component: ErrorComponent}
